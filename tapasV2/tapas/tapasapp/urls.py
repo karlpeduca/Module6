10fc3660eca0d10279b256c577bb83d271f9cf30
karlpeduca <karl.nicholai.peduca@student.ateneo,edu>
2026-04-10 00:58:03 +0800

    html changes: basic_list, login, signup

--- tapasV2/tapas/tapasapp/urls.py
+++ tapasV2/tapas/tapasapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
     path('better_menu', views.better_menu, name='better_menu'),
     path('add_menu', views.add_menu, name='add_menu'),
     path('view_detail/<int:pk>/', views.view_detail, name='view_detail'),
